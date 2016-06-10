88e001d818cde79994271bccba276e119bd9f657
Mark Tiedemann <www.marktiedemann@gmail.com>
2016-06-10 22:43:54 +0300

    Switches to String.repeat() for indent blanks

--- wrap.js
+++ wrap.js
@@ -2,13 +2,11 @@
 
 const wordWrap = require('word-wrap')
 
-const blanks = n => new Array(n + 1).join(' ')
-
 module.exports = ({ message = '', indentLeft = 4, indentRight = 4, indentFirstLine = false } = {}) => {
 
     const lines = wordWrap(message, {
         width: process.stdout.columns - indentLeft - indentRight,
-        indent: blanks(indentLeft),
+        indent: ' '.repeat(indentLeft),
         trim: true
     })
 
